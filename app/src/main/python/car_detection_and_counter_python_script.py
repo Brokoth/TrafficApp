7446fe53519149c7f967a4e9fadaeac4e644469a
Brokoth <42600065+Brokoth@users.noreply.github.com>
2021-01-16 18:32:34 +0300

    Update car_detection_and_counter_python_script.py

--- app/src/main/python/car_detection_and_counter_python_script.py
+++ app/src/main/python/car_detection_and_counter_python_script.py
@@ -15,32 +15,39 @@ def car_detection(car_vid_url):
     best = video.getbest()
     play_url = best.url
     camera = cv2.VideoCapture(play_url)
-    print(camera.get(cv2.CAP_PROP_FPS))
+    fps = camera.get(cv2.CAP_PROP_FPS)
+    frame_count = int(camera.get(cv2.CAP_PROP_FRAME_COUNT))
+    duration = frame_count / fps
     count = 0
+    print(count, ',', frame_count, ',', fps, ',', duration)
     while True:
         ret, img = camera.read()
-        height, width = img.shape[0:2]
-        img[0:70, 0:width] = [0, 0, 255]
-        cv2.putText(img, 'CAR COUNT:', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 255, 255), 2)
-        cv2.line(img, (0, height - 200), (width, height - 200), (0, 255, 255), 2)
-        blur = cv2.blur(img, (3, 3))
-        gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
-        cars = cars_classifier.detectMultiScale(gray)
-        for (x, y, w, h) in cars:
-            car_cy = int(y + h / 2)
-            line_cy = height - 200
-            if line_cy + 6 > car_cy > line_cy - 6:
-                count = count + 1
-            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.putText(img, 'CAR', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-            cv2.putText(img, str(count), (500, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        cv2.imshow('live', img)
-        key = cv2.waitKey(1)
-        if key == 27:
+        if img is not None:
+            height, width = img.shape[0:2]
+            img[0:70, 0:width] = [0, 0, 255]
+            cv2.putText(img, 'CAR COUNT:', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 255, 255), 2)
+            cv2.line(img, (0, height - 200), (width, height - 200), (0, 255, 255), 2)
+            blur = cv2.blur(img, (3, 3))
+            gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
+            cars = cars_classifier.detectMultiScale(gray)
+            for (x, y, w, h) in cars:
+                car_cy = int(y + h / 2)
+                line_cy = height - 200
+                if line_cy + 6 > car_cy > line_cy - 6:
+                    count = count + 1
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cv2.putText(img, 'CAR', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                cv2.putText(img, str(count), (500, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            cv2.imshow('live', img)
+            key = cv2.waitKey(1)
+            if key == 27:
+                break
+        else:
             break
     cv2.destroyAllWindows()
     camera.release()
-    return count
+    print(count, ',', frame_count, ',', fps, ',', duration)
+    return count, ',', frame_count, ',', fps, ',', duration
 
 
 vid_url = "https://www.youtube.com/watch?v=Y1jTEyb3wiI&list=PLxgQHbsjYVOhmhdGps5Fiw16Av3Ni1Kz0&index=22"
